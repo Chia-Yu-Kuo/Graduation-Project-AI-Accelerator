c2b16b7900afd3cda65450b2cda32c449894e9a8
Chia-Yu-Kuo <144416821+Chia-Yu-Kuo@users.noreply.github.com>
2023-10-08 14:05:11 +0800

    Delete src/hw directory

--- src/hw/fpga/ReLU.v
+++
@@ -1,9 +0,0 @@
-`include "Define.v"
-
-module ReLU (data_in,data_out);
-    input signed [`psum_wid-1:0] data_in;
-    output signed [`psum_wid-1:0] data_out;
-
-    assign data_out = (data_in > $signed('d0)) ? data_in : 'd0;
-
-endmodule
